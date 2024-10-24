3e2acfb7bc4fba69ed4c7c8fc065aa120420f7aa
Abdulrofihi Musa <m.abdulrofihi@gmail.com>
2024-10-24 16:28:48 +0100

    Define basic operations and operation variables

---
+++ scripts.js
@@ -0,0 +1,24 @@
+// addition
+function add(a, b) {
+    return a + b;
+}
+
+// subtraction
+function subtract(a, b) {
+    return a - b;
+}
+
+// multiplication
+function multiply(a, b) {
+    return a * b;
+}
+
+// division
+function divide(a, b) {
+    return a / b;
+}
+
+// operation variables
+let firstNum;
+let secondNum;
+let operator;
