dbec38afed18d8d1d3b55f20bd0ca63a12cb9d1c
Abdulrofihi Musa <m.abdulrofihi@gmail.com>
2025-01-14 18:23:07 +0100

    Add operate function

--- scripts.js
+++ scripts.js
@@ -1,3 +1,9 @@
+// operation variables
+let firstNum;
+let secondNum;
+let operator;
+let operators = ["+", "-", "*", "/"];
+
 // addition
 function add(a, b) {
     return a + b;
@@ -18,7 +24,21 @@ function divide(a, b) {
     return a / b;
 }
 
-// operation variables
-let firstNum;
-let secondNum;
-let operator;
+function operate(operator, firstNum, secondNum) {
+    switch (operator) {
+        case "+":
+            add(firstNum, secondNum);
+            break;
+        case "-":
+            subtract(firstNum, secondNum);
+            break;
+        case "*":
+            multiply(firstNum, secondNum);
+            break;
+        case "/":
+            divide(firstNum, secondNum);
+            break;
+        default:
+            alert("Incorrect operator");
+    }
+}
